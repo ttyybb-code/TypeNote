6705e15ddd9c396ce0ba27aa3d2568a65866c9c0
ttyybb-code <128849581+ttyybb-code@users.noreply.github.com>
2024-10-10 16:22:32 -0600

    now makes a file

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-{\rtf1}
+import * as fs from 'fs';
+import * as path from 'path';
+
+function getString(message: string) {
+        const userInput: string | null = prompt(message + ': ');
+        return userInput !== null ? userInput : "";
+}
+
+var fileName: string = getString("Enter the name of your note")
+
+const filePath: string = path.join(__dirname, '../notes', fileName);
+
+var note: string = getString("Enter your notes")
+fs.writeFile(filePath, note, (err) => {
+        if (err) {
+                console.error("Error writing file:", err);
+        } else {
+                console.log("File written successfully in the 'notes' folder!");
+        }
+});
+
